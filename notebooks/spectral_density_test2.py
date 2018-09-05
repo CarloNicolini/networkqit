07d00d1716067456ae0488c9842aabbc4abd0a3b
ottino <nicolinidisi@gmail.com>
2018-09-05 21:33:54 +0200

    computation with analytical spectral density

--- notebooks/spectral_density_test2.py
+++ notebooks/spectral_density_test2.py
@@ -193,48 +193,42 @@ def compute_rho(allz, ers, nr, matrix, eps=1E-7, maxsteps=150, include_isolated=
     else:
         return allz, rho
 
-# def test1(matrix):
-#     b0 = 2
-#     pin, pout = 0.8, 0.2
-#     sigma2rs = pout*(np.ones(b0)-np.eye(b0)) + pin*np.eye(b0)
-#     #M0 = np.array([[0.8,0.2],[0.3,0.5]])
-#     #sigma2rs = sigma2rs#np.kron(M0, M0)
 
-#     nr = [100,50]
-#     b = len(nr)
-#     nrns = np.reshape(np.kron(nr,nr),[b,b])
-#     ers = np.multiply(sigma2rs,nrns)
+def rho_z(z, ers, nr, matrix, eps=1E-7, t0=None):
+    #print('z=',z)
+    if t0 is None:
+        # use a default initial value for the stieltjes transform tr
+        t0 = [mp.mpc('0')] * b
 
-#     # Empirical eigenvalues
-#     reps = 50
-#     if matrix is 'laplacian':
-#         eigs = np.array([scipy.linalg.eigvalsh(GL(hierarchical_random_graph2(ers, nr))) for i in range(0, reps)]).flatten()
-#     elif matrix is 'adjacency':
-#         eigs = np.array([scipy.linalg.eigvalsh(hierarchical_random_graph(ers, nr)) for i in range(0, reps)]).flatten()
-#     elif matrix is 'norm_laplacian':
-#         eigs = np.array([scipy.linalg.eigvalsh(NGL(hierarchical_random_graph(ers, nr))) for i in range(0, reps)]).flatten()
+    N = np.sum(nr)  # total number of nodes
+    b = ers.shape[0]
+    t = compute_tr(z + eps * 1j, t0, ers, nr, matrix)
+    rho = -(1.0 / (N * np.pi)) * np.sum([nr[r] * t[r].imag for r in range(0, b)])
+    return rho
 
-#     allz, rho = compute_rho(np.linspace(-50, 50, 1000), sigma2rs, nr, matrix, eps=1E-12, t0=[1E-5j]*ers.shape[0])
-#     #plt.figure()
-#     #plt.hist(eigs, bins=200)
-#     #plt.figure()
-#     #plt.plot(allz, rho)
+def expected_partition_function(ers,nr,beta,reps):
+    Z = 0
+    for i in range(0,reps):
+        L = GL(hierarchical_random_graph2(ers, nr))
+        Z += np.exp(-beta*scipy.linalg.eigvalsh(L)).sum()
+    Z /= reps
+    return Z
 
-#     plt.figure()
-#     plt.hist(eigs, density=1,bins=200)
-#     plt.plot(allz, rho)
-#     plt.show()
-
+def analytical_partition_function(ers,nr,beta,):
+    f = lambda z : mp.exp(-beta*z)*rho_z(z,ers,nr,'laplacian',t0=[mp.mpc('0')] * len(nr))
+    I = mp.quad(f,[0,mp.inf])
+    return I * np.sum(nr)
 
 def test2(matrix):
     ers = np.array([[5000, 25000], [25000, 5000]])
-    ers = np.kron(ers,np.array([[1,0.8],[0.8,0.5]])*2)
+    #ers = np.kron(ers,np.array([[1,0.8],[0.8,0.5]])*2)
     print(ers)
-    nr = [512, 256]*2
+    nr = [512, 256]#*2
     b = len(nr)
     nrns = np.reshape(np.kron(nr, nr), [b, b])
     reps = 5
     print(ers/nrns)
+
     if matrix is 'laplacian':
         eigs = np.array([scipy.linalg.eigvalsh(GL(hierarchical_random_graph2(ers, nr))) for i in range(0, reps)]).flatten()
     elif matrix is 'adjacency':
@@ -256,7 +250,7 @@ def test2(matrix):
     plt.subplot(1,3,3)
     plt.hist(eigs, density=1, bins=500)
     plt.show()
-    
+
     np.savetxt('ers.dat',ers)
     np.savetxt('nrns.dat',nrns)
     np.savetxt('eigs.dat',eigs)
@@ -269,6 +263,7 @@ def test2(matrix):
     #plt.vlines(x=zmax, ymin=0, ymax=0.05, color='b')
     plt.plot(allz, np.abs(rho))
     plt.show()
+
     print()
     A=np.loadtxt('data.dat')
     for i in np.arange(0,2*len(nr),2):
@@ -276,5 +271,16 @@ def test2(matrix):
     plt.show()
 
 
+def test3(matrix):
+    ers = np.array([[5000, 25000], [25000, 5000]])
+    nr = [512, 256]#*2
+    b = len(nr)
+    nrns = np.reshape(np.kron(nr, nr), [b, b])
+    reps = 5
+    
+    beta=1E-2
+    print('E[Tr[exp(-bL)]]=', expected_partition_function(ers,nr,beta,reps))
+    print('Integral=', analytical_partition_function(ers,nr,beta))
+
 if __name__ == '__main__':
-    test2('laplacian')
+    test3('laplacian')
