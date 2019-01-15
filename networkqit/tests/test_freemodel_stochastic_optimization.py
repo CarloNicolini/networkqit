13d8a0a6eb70fa39b1e128ba9ed97e71050f6faa
carlo <carlo.nicolini@iit.it>
2019-01-15 16:57:00 +0100

    adding support for batched gradient descent and adam

---
+++ networkqit/tests/test_freemodel_stochastic_optimization.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append('/home/carlo2/workspace/networkqit')
+import matplotlib.pyplot as plt
+import autograd
+from autograd import numpy as np
+import networkqit as nq
+from networkqit.algorithms.stochastic_optimize_autograd import StochasticGradientDescent, Adam
+import networkx as nx
+#A = np.loadtxt('/home/carlo2/workspace/communityalg/data/karate.adj')
+A = nx.to_numpy_array(nq.ring_of_cliques(5,5))
+N = len(A)
+M = nq.FreeModel(N=N)
+p = A.sum() / (N*(N-1))
+L = nq.graph_laplacian(A)
+
+beta = 5.0
+opt = Adam(A=A, L=L, x0=np.random.random([N*N,]), beta_range=[beta])
+rho = nq.VonNeumannDensity(A=None, L=L, beta=beta).density
+opt.setup(model=M)
+
+#G = autograd.grad(lambda x : np.sum(M.sample_adjacency(x)))
+#x0 = np.random.random([34,])
+
+#G = opt.gradient(x=np.random.random([N,1]), rho=rho, beta=beta, num_samples=1)
+sol = opt.run(eta=1E-1, max_iters=np.inf, gtol=1E-5)
+plt.pause(5)
+plt.show()
