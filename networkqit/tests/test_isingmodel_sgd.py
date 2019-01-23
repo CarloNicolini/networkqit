69ce15abeec6c63daa3b3189a976321f12efaa56
carlotds <carlo@carlotds>
2019-01-23 18:09:02 +0100

    refactoring code

--- networkqit/tests/test_isingmodel_sgd.py
+++ networkqit/tests/test_isingmodel_sgd.py
@@ -18,14 +18,14 @@ p = A.sum() / (N*(N-1))
 L = nq.graph_laplacian(A)
 
 beta = 1
-opt = Adam(A=A, L=L, x0=np.random.random([N*N,]), beta_range=np.logspace(0.5,-3,100), model=M)
+opt = Adam(A=A, L=L, x0=np.random.random([N*N,]), beta_range=np.logspace(3,-3,100), model=M)
 rho = nq.compute_vonneuman_density(L=L, beta=beta)
 
 #G = autograd.grad(lambda x : np.sum(M.sample_adjacency(x)))
 #x0 = np.random.random([34,])
 
 #G = opt.gradient(x=np.random.random([N,1]), rho=rho, beta=beta, num_samples=1)
-sol = opt.run(refresh_frames=2000, quasi_hyperbolic=True, eta=1E-3, max_iters=1000, gtol=1E-5, batch_size=128)
+sol = opt.run(refresh_frames=100, quasi_hyperbolic=True, eta=1E-3, max_iters=1000, gtol=1E-5, batch_size=16)
 
 plt.pause(5)
 plt.show()
