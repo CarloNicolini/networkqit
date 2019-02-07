fixed uwcm loglikelihood

networkqit/algorithms/optimize.py
@@ -179,7 +179,7 @@ class MLEOptimizer(ModelOptimizer):
                 self.sol = minimize(fun=lambda z: -self.model.loglikelihood(self.G, z),
                                     x0=np.squeeze(self.x0),
                                     method='L-BFGS-B',
-                                    #jac=J,
+                                    jac=J,
                                     bounds=self.model.bounds,
                                     options=opts)
             if kwargs.get('verbose',0)>0:

networkqit/graphtheory/models/MEModels.py
@@ -142,11 +142,11 @@ class UWCM(GraphModel):
         self.bounds = [(EPS, 1.0-EPS) for i in range(0, self.N)]
 
     def expected_adjacency(self, *args):
-        pij = np.outer(args, args)
+        pij = np.outer(*args, *args)
         return pij
     
     def expected_weighted_adjacency(self, *args):
-        pij = self.expected_adjacency(args)
+        pij = self.expected_adjacency(*args)
         wij = pij / (1.0 - pij)
         return wij
     

networkqit/tests/test_uwcm_fitting.py
@@ -71,7 +71,7 @@ if __name__=='__main__':
 
     M = UWCM(N=len(W))
     x0 = (np.concatenate([s])+1E-5)*1E-3 #+ (np.random.random([2*len(W),])*2-1)*1E-5
-
+    print(x0.shape)
     # Optimize by L-BFGS-B
     opt = nq.MLEOptimizer(W, x0=x0, model=M)
     sol = opt.run(model=M, verbose=0,gtol=1E-8, method='MLE')
@@ -81,16 +81,16 @@ if __name__=='__main__':
     wij = M.expected_weighted_adjacency(sol['x'])
     plot(W,pij,wij)
 
-    nq.MLEOptimizer(W, x0=sol['x'], model=M)
-    sol = opt.run(method='saddle_point', xtol=1E-12, gtol=1E-9)
-    print('Loglikelihood = ', M.loglikelihood(G,sol['x']))
-    pij = M.expected_adjacency(sol['x'])
-    wij = M.expected_weighted_adjacency(sol['x'])
-    plot(W,pij,wij)
+    # opt = nq.MLEOptimizer(W, x0=x0, model=M)
+    # sol = opt.run(method='saddle_point', xtol=1E-12, gtol=1E-9)
+    # print('Loglikelihood = ', M.loglikelihood(G,sol['x']))
+    # pij = M.expected_adjacency(sol['x'])
+    # wij = M.expected_weighted_adjacency(sol['x'])
+    # plot(W,pij,wij)
 
-    sol = opt.run(method='saddle_point', basinhopping = True, basin_hopping_niter=10, xtol=1E-9, gtol=1E-9)
-    #print('Gradient at Least squares solution=\n',grad(sol['x']))
-    print('Loglikelihood = ', M.loglikelihood(G,sol['x']))
-    pij = M.expected_adjacency(sol['x'])
-    wij = M.expected_weighted_adjacency(sol['x'])
-    plot(W,pij,wij)
+    # sol = opt.run(method='saddle_point', basinhopping = True, basin_hopping_niter=10, xtol=1E-9, gtol=1E-9)
+    # #print('Gradient at Least squares solution=\n',grad(sol['x']))
+    # print('Loglikelihood = ', M.loglikelihood(G,sol['x']))
+    # pij = M.expected_adjacency(sol['x'])
+    # wij = M.expected_weighted_adjacency(sol['x'])
+    # plot(W,pij,wij)