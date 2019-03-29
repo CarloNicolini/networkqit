4ab487822ddcfaaf7c58752abf3ba4aa6531aa32
carlotds <carlo@carlotds>
2019-03-29 16:22:27 +0100

    removed conflicts

--- networkqit/tests/test_isingmodel_sgd.py
+++ networkqit/tests/test_isingmodel_sgd.py
@@ -19,22 +19,14 @@ p = A.sum() / (N*(N-1))
 L = nq.graph_laplacian(A)
 
 beta = 1
-<<<<<<< HEAD
-opt = Adam(A=A, L=L, x0=np.random.random([N*N,]), beta_range=np.logspace(3,-3,100), model=M)
-=======
 opt = Adam(A=A, L=L, x0=np.random.random([N*N,]), beta_range=np.logspace(-0.5,-3,100), model=M)
->>>>>>> bf8cabdf2849d0d61d09282c4fdc13d3917936e2
 rho = nq.compute_vonneuman_density(L=L, beta=beta)
 
 #G = autograd.grad(lambda x : np.sum(M.sample_adjacency(x)))
 #x0 = np.random.random([34,])
 
 #G = opt.gradient(x=np.random.random([N,1]), rho=rho, beta=beta, num_samples=1)
-<<<<<<< HEAD
 sol = opt.run(refresh_frames=100, quasi_hyperbolic=True, eta=1E-3, max_iters=1000, gtol=1E-5, batch_size=16)
-=======
-sol = opt.run(refresh_frames=50, eta=2E-3, max_iters=2000, gtol=1E-5, batch_size=32)
->>>>>>> bf8cabdf2849d0d61d09282c4fdc13d3917936e2
 
 plt.pause(5)
 plt.show()
